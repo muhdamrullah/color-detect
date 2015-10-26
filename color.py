a3be1a85f72256b9544177df068a76bb9b253864
muhdamrullah <muneorion@gmail.com>
2015-10-26 10:04:48 +0000

    Update color.py

--- color.py
+++ color.py
@@ -5,10 +5,20 @@ color_thief = ColorThief('/var/lib/motion/Photo.jpg')
 # get the dominant color
 dominant_color = color_thief.get_color(quality=1)
 
+r = float(dominant_color[0])
+g = float(dominant_color[1])
+b = float(dominant_color[2])
+print r,g,b
+h, s, v = colorsys.rgb_to_hsv(r, g, b)
+print h
+print s
+print v
+s = s*5
+v = v*1.5
 
+r, g, b = colorsys.hsv_to_rgb(h, s, v)
 
-#converts rgb to hls format
-
+print r,g,b
 # build a color palette
 palette = color_thief.get_palette(color_count=6)
 print palette
@@ -19,7 +29,7 @@ with open('myStyles.css', 'r') as file:
     data = file.readlines()
 
 # now change the 2nd line, note that you have to add a newline
-data[3] = '    background: rgb(%d, %d, %d);\n' % dominant_color
+data[3] = '    background: rgb(%d, %d, %d);\n' % (r,g,b)
 
 # and write everything back
 with open('MyStyles.css', 'w') as file:
